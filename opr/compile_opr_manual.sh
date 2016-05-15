ed01cab0b61f281c1a2b6652f148a3c54dc24d43
Artem Abramov <temach@gmail.com>
2016-05-15 18:44:52 +0300

    Added operator manual documentation.

---
+++ opr/compile_opr_manual.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+fname=$(basename -s .tex $1)
+pdflatex -synctex=1 -interaction=nonstopmode ${fname}.tex
+# swap the title page for custom title page
+pdftk A=artem_title_page.pdf B=${fname}.pdf cat A B2-end output temp_compile_file_1.pdf
+mv "temp_compile_file_1.pdf" ${fname}.pdf
+# add the lsit of changes to the back
+# pdftk A=list_of_changes.pdf B=${fname}.pdf cat B A output temp_compile_file_1.pdf
+# mv "temp_compile_file_1.pdf" ${fname}.pdf
+# show but detatch the process and without debug output
+evince ${fname}.pdf &
